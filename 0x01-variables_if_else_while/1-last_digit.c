d0effc7cef2076e4442ace6ff5c39db44167a8c9
Hacker <hajjimohamedreda1@gmail.com>
2023-06-16 17:25:50 +0100

    $commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,21 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Determine if a number is grater than 5 or not
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5 )
+	if (n > 5)
 		printf("is %d and is greater than 5", n);
 	else if (n == 0)
 		printf("is %d and is 0", n);
-	else 
+	else
 		printf("is %d and is less than 6 and not 0", n);
 	return (0);
 }

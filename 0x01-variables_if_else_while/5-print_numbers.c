7d3c49cc3a1049bb44c6be2b388ea054532e2e41
Hacker <hajjimohamedreda1@gmail.com>
2023-06-16 19:13:02 +0100

    $commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase then uppercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+}
+	int i;
+
+	for (i = 0; i <= 10; i++);
+	{
+	printf("%d", i);
+}
+printf("\n");
+return (0);
+}

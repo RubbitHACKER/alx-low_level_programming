b11d12ccb79faddc8d71e3262cd694e464430e79
Hacker <hajjimohamedreda1@gmail.com>
2023-06-16 23:44:17 +0100

    $commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio>
+/**
+ * main - A program that prints the numbers from 0 to 9 then alphabit
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+{
+	putchar(i);
+{
+	for (i = 44; i == 44; i++)
+	putchar(i);
+}
+putchar('36');
+putchar('\n');
+return (0);
+}

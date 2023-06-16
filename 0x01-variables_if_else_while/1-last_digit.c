0e1a09ffb16a5b0e8a887863325ec8a5ec265275
Hacker <hajjimohamedreda1@gmail.com>
2023-06-16 17:44:04 +0100

    $commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
-		printf("is %d and is greater than 5", n);
-	else if (n == 0)
-		printf("is %d and is 0", n);
+	if ((n % 10) > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
+	else if ((n % 10) < 6 && (n % 10) != 0)
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	else
-		printf("is %d and is less than 6 and not 0", n);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	return (0);
 }

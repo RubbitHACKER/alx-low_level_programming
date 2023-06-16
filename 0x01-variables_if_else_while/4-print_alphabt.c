ab2d10093edca41fc9b04dede87897c133175df1
Hacker <hajjimohamedreda1@gmail.com>
2023-06-16 18:59:01 +0100

    $commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase then uppercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char alph[24] = "abcdfghijklmnopqrstuvwxyz";
+
+for (i = 0; i < 24; i++)
+{
+putchar(alph[i]);
+}
+	putchar("\n");
+	return (0);

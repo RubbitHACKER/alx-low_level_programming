bb182a0b1219953df195a120f3ac0a25ce3f0993
Hacker <hajjimohamedreda1@gmail.com>
2023-06-16 23:34:40 +0100

    $commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,15 @@
 int main(void)
 {
 	int i;
-	char alp[6] = "abcdef";
 
 	for (i = 48; i < 58; i++)
 {
 		putchar(i);
 }
-for (i = 97; i < 103; i++)
+	for (i = 97; i < 103; i++)
 {
-putchar(i);
+	putchar(i);
 }
-putchar('\n');
+	putchar('\n');
 return (0);
 }

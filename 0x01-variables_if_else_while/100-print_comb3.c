183979b470cad567e4e7d1c98fcf6f96907672d4
Hacker <hajjimohamedreda1@gmail.com>
2023-06-17 01:24:51 +0100

    $commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - print all possible different combinition
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{	
+	int i,j;
+	for (i = 48; i <= 56; i++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			if (m < n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

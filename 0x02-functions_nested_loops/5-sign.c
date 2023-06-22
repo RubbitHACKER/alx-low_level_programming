1afc546c257910b341b4e9650ef37bfec872aa22
root <root@Rubbit.localdomain>
2023-06-22 02:56:51 +0100

    holla

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_sign - print the sign of a number
- *  @c: is the char to check
- *  return: 1 and print + if n is greater than zero
- *  0 and prints 0 if n is 0
- *  -1 and prints - if n less than zero
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -14,4 +14,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }

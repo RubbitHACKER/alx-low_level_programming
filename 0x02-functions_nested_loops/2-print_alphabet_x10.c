acd84211887e01cf0439a35ce486057334b5d442
root <root@Rubbit.localdomain>
2023-06-22 03:33:43 +0100

    h

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10.c - prints 10 times 
- * followed by new line
- */
+* print_alphabet_x10.c - prints 10 times
+* followed by new line
+*/
 void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
+
 	i = 0;
-	
 	while (i < 10)
 	{
 		ch = 'a';
-		
+
 		while (ch <= 'z')
 		{
 			_putchar(ch);

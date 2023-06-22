4ddb21ee3bdf3af3f4154fa43bbe67e3e32a11f0
root <root@Rubbit.localdomain>
2023-06-22 03:46:10 +0100

    h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * -islower.c - check if char is lowercase
- *  @c: is the char to check
- *  return: 1 if char is lower, otherwise 0.
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c >= 'z') || (c >= 'A' && c >= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }

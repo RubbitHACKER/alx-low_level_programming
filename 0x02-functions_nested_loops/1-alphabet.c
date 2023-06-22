74b0a237feb3a0b4cd6d01483b62d31785e91f50
root <root@Rubbit.localdomain>
2023-06-22 02:52:38 +0100

    hello

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
-
 /**
- * print_alphabet - print all alphabet lowercase
+ * print_alphabet - print all alphabet in lowercase
  */
-int main(void)
+
+void print_alphabet(void)
 {
-    char letter;
-    
-    for (letter = 'a'; letter <= 'z'; letter++)
-	    _putchar(letter);
-    	_putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
 }

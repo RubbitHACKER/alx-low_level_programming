a1ee558522224a2fdbbf17681ab0c390a7683422
root <root@Rubbit.localdomain>
2023-06-22 03:44:12 +0100

    h

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * -islower.c - check if char is lowercase
- *  @c: is the char to check
- *  return: 1 if char is lower, otherwise 0.
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
  */
 
 int _islower(int c)
 {
-	if (c >= 'a' && c >= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);

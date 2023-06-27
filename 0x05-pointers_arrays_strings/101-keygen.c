be384cd60ac35c3c687c060c8c55f927d0c70206
root <root@Rubbit.localdomain>
2023-06-27 01:25:27 +0100

    h

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,10 +19,10 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 78; 
-		sum += (pass[i] + '0'); 
-		putchar(pass[i] + '0'); 
-		
+		pass[i] = rand() % 78;
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;

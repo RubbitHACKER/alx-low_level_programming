be49e108dfbb31adabeb3e26bd46ea4e3b8444a4
root <root@Rubbit.localdomain>
2023-06-29 20:20:03 +0100

    h

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * _strcmp - compare values
- *@s1:input
- @s2:input
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
  * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
